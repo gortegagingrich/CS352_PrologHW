0791c0fe5f3b7f11656065ed5a342a55fd4c16a6
Gabriel <gortegagingrich@gmail.com>
2017-04-08 08:56:20 -0700

    cleaned stuff up

--- Homework1.pl
+++ Homework1.pl
@@ -1,18 +1,26 @@
-isList([]).
-isList(X) :- [_|_]=X.
 
-prepend([],B,X) :- B=X.
-prepend(A,B,X) :- [A|B]=X.
+isList([_|_]).
+
+prepend(A,B,[A|B]).
 
 remove(_,[],[]).
 remove(A,[A|T],T).
-remove(A,[H|T],[H|X]) :- remove(A,T,X).
+remove(A,[H|T],[H|X]) :-
+	remove(A,T,X).
 
 filter(_,[],[]).
-filter(A,[A|T],R) :- filter(A,T,R).
-filter(A,[H|T],[H|X]) :- filter(A,T,X).
+filter(A,[A|T],R) :-
+	filter(A,T,R).
+filter(A,[H|T],[H|X]) :-
+	filter(A,T,X).
 
-frenchFlag(A, X) :- getAs(r,A,[],R), getAs(w,A,R,W), getAs(b,A,W,X).
-getAs(A,[A|T],Acc,X) :- getAs(A,T,[A|Acc],X).
-getAs(A,[_|T],Acc,X) :- getAs(A,T,Acc,X).
+frenchFlag(A, X) :-
+	getAs(r,A,[],R),
+	getAs(w,A,R,W),
+	getAs(b,A,W,X).
+
+getAs(A,[A|T],Acc,X) :-
+	getAs(A,T,[A|Acc],X).
+getAs(A,[_|T],Acc,X) :-
+	getAs(A,T,Acc,X).
 getAs(_,[],Acc,Acc).

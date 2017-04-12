eb6b365b143f8b7ad18d1d2ad8f43bf1db72b05a
Gabriel Ortega-Gingrich <gortegagingrich@gmail.com>
2017-04-11 19:42:41 -0700

    redid 5th exercise and fixed 3 and 4

--- Homework1.pl
+++ Homework1.pl
@@ -1,26 +1,35 @@
-
 isList([_|_]).
 
 prepend(A,B,[A|B]).
 
 remove(_,[],[]).
-remove(A,[A|T],T).
+remove(A,[A|T],T) :- !.
 remove(A,[H|T],[H|X]) :-
-	remove(A,T,X).
+	remove(A,T,X), !.
 
 filter(_,[],[]).
 filter(A,[A|T],R) :-
-	filter(A,T,R).
+	filter(A,T,R),
+	!.
 filter(A,[H|T],[H|X]) :-
-	filter(A,T,X).
+	filter(A,T,X),
+	!.
 
 frenchFlag(A, X) :-
-	getAs(r,A,[],R),
-	getAs(w,A,R,W),
-	getAs(b,A,W,X).
+	flag(A,[],[],[],X),
+	!.
 
-getAs(A,[A|T],Acc,X) :-
-	getAs(A,T,[A|Acc],X).
-getAs(A,[_|T],Acc,X) :-
-	getAs(A,T,Acc,X).
-getAs(_,[],Acc,Acc).
+flag([], Acc1, Acc2, Acc3, X) :-
+	append(Acc1,Acc2,X0),
+	append(X0,Acc3,X).
+flag([H|T], Acc1, Acc2, Acc3,X) :-
+	H=b,
+	flag(T, [H|Acc1], Acc2, Acc3, X).
+flag([H|T], Acc1, Acc2, Acc3, X) :-
+	H=w,
+	flag(T, Acc1, [H|Acc2], Acc3, X).
+flag([H|T], Acc1, Acc2, Acc3, X) :-
+	H=r,
+	flag(T, Acc1, Acc2, [H|Acc3],X).
+flag([_|T], Acc1, Acc2, Acc3, X) :-
+	flag(T, Acc1, Acc2, Acc3, X).

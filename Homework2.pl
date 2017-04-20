f23b51ce8e41d045c64093f20d99a898a46637b7
Gabriel <gortegagingrich@gmail.com>
2017-04-19 21:52:32 -0700

    path works now

--- Homework2.pl
+++ Homework2.pl
@@ -1,14 +1,16 @@
 %predicate 1
 path(X,X,[]).
 path(X,Y,P) :-
-	path2(X,Y,[],P).
+	path2(X,Y,[],[],P1),
+	reverse(P1,[],P).
 
-path2(X, X, Acc, Acc).
-path2(X, Y, Acc, P) :-
+path2(X, X, _, Acc, [X|Acc]).
+path2(X, Y, Visited, Acc, P) :-
 	X \== Y,
+	notContains(X,Visited),
 	doorExists(X,X2),
-	notContains(X2, P),
-	path2(X2,Y,[X|Acc],P).
+	path2(X2,Y,[X|Visited],[X|Acc], P).
+
 
 doorExists(X,Y) :-
 	door(X,Y);
@@ -18,3 +20,7 @@ notContains(_, []).
 notContains(X, [H|T]) :-
 	H \== X,
 	notContains(X,T).
+
+reverse([], Acc, Acc).
+reverse([H|T], Acc, X) :-
+	reverse(T,[H|Acc], X).

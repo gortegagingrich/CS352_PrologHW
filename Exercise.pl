e31ef2ddaa613742b958b7d1881e261a1690df85
Gabriel Ortega-Gingrich <gortegagingrich@gmail.com>
2017-04-25 18:22:38 -0700

    did first three

---
+++ Exercise.pl
@@ -0,0 +1,45 @@
+%% 1. isbefore/2
+%% true if first arguent is lexically before second
+isbefore(X,X).
+isbefore(X,Y) :-
+	X \== Y,
+	atom(X),
+	atom(Y),
+	atom_codes(X,XX),
+	atom_codes(Y,YY),
+	isbefore(XX,YY).
+isbefore([],[_|_]).
+isbefore([X|_],[Y|_]) :-
+	X < Y.
+isbefore([X|XX], [X|YY]) :-
+	isbefore(XX,YY).
+
+%% 2. split/4
+%% takes atom and list and returns two lists (less than atom, greater than or equal to atom)
+split(A, L, X, Y) :-
+	split2(A,L,[],[],X,Y).
+	
+split2(_,[],Lt,Geq,Lt,Geq).
+split2(A,[H|T],Lt,Geq,X,Y) :-
+	isbefore(H,A),
+	split2(A,T,[H|Lt],Geq,X,Y).
+split2(A,[H|T],Lt,Geq,X,Y) :-
+	not(isbefore(H,A)),
+	split2(A,T,Lt,[H|Geq],X,Y).
+
+%% 3. qsort/2
+%% second list is first list sorted using quicksort
+%% use head as pivot
+%% split list into smaller and bigger
+%% recursively sort smaller and bigger
+%% use append to put list back together with pivot in middle
+qsort([],[]).
+qsort([H],[H]).
+qsort([H|T],Y) :-
+	T \== [],
+	split(H,T,Lo,Hi),
+	qsort(Lo,Y1),
+	qsort(Hi,Y2),
+	append(Y1,[H|Y2],Y).
+
+%% 4. term2list/2
